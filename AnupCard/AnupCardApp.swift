5f2df365785d72446f5caa32e21b05db3ec0105f
Anup Saud <anupsaud10@gmail.com>
2024-07-01 09:59:54 -0400

    Initial Commit

---
+++ AnupCard/AnupCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnupCardApp.swift
+//  AnupCard
+//
+//  Created by Anup Saud on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct AnupCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
